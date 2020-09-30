4d934ff2a9547e52ba4b48018c70415fd2dae12b
Vector412 <61909904+Vector412@users.noreply.github.com>
2020-09-30 11:52:20 +0300

    меню, работа с цифрами, нажатия

---
+++ BiotixProject/Assets/Scripts/HandleScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HandleScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler//IPointerEnterHandler
+{
+    [SerializeField] Text LogText;
+    int touch;
+    bool one, two;
+
+    private void Start()
+    {
+        touch = 0;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Global touch!");
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("No Global touch!");
+
+        //InputManager.Instance.InputKey(true);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        LogText.transform.position = eventData.position;
+    }
+
+    /*  public void OnPointerEnter(PointerEventData eventData)
+      {
+          Debug.Log(gameObject);
+      }*/
+
+    private void OnMouseDown()
+    {
+
+        touch++;
+        Debug.Log(touch);
+
+        if (touch == 2 && gameObject)
+
+        {
+            Debug.Log(touch);
+            var twoGameObj = this.gameObject;
+            Cell cell2 = twoGameObj.GetComponent<Cell>();
+            if (cell2.currentCountCell > 0)
+            {
+                Debug.Log(cell2.currentCountCell);
+                cell2.currentCountCell += 10;
+            }
+        }
+        else if (gameObject)
+        {
+            var mainGameobj = this.gameObject;
+            Cell cell1 = mainGameobj.GetComponent<Cell>();
+            if (cell1.currentCountCell > 0)
+            {
+                Debug.Log(cell1.currentCountCell);
+                cell1.MigrateCountCell();
+            }
+        }
+    }
+}
---
+++ BiotixProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : GenericSingletonClass<InputManager>
+{
+    public Action Activate = delegate { };
+
+   /* public void Update()
+    {
+        InputKey();
+
+    }*/
+    public void InputKey( bool isActive)
+    {
+        if (isActive)
+        {
+            Activate();
+        }
+      
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }*/
+    }
+
+
+}
