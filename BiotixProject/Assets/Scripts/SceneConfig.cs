1dec3fa5e4c0d170c8baf7e3502fc61dcce17a2a
Vector412 <61909904+Vector412@users.noreply.github.com>
2020-10-15 23:33:41 +0300

    левел дизайн, пауза, главное меню

---
+++ BiotixProject/Assets/Scripts/SceneConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneConfig : MonoBehaviour
+{
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+    }
+    public void ActiveMenu()
+    {
+        Time.timeScale = 0;
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+
+}
